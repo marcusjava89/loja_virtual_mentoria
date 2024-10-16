27bc2eefa2c62851ff9dfb4f8c77a205c47fce38
Marcus Martins <marcusjava89@gmail.com>
2024-10-16 11:04:44 -0300

    commit inicial

--- Documents/workspace-spring-tool-suite-4-4.25.0.RELEASE/loja_virtual_mentoria/src/main/java/jdev/mentoria/lojavirtual/LojaVirtualMentoriaApplication.java
+++ Documents/workspace-spring-tool-suite-4-4.25.0.RELEASE/loja_virtual_mentoria/src/main/java/jdev/mentoria/lojavirtual/LojaVirtualMentoriaApplication.java
@@ -3,13 +3,11 @@ package jdev.mentoria.lojavirtual;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/*A anotação abaixo é o que faz o Springboot funcionar*/
 @SpringBootApplication
 public class LojaVirtualMentoriaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LojaVirtualMentoriaApplication.class, args);
-		System.out.println("Conexão loja virtual mentoria.");
 	}
 
 }
