ae1f418d36febc2f5623467831792a217ec99cdb
Marcus Martins <marcusjava89@gmail.com>
2024-12-04 12:21:21 -0300

    mudança versão do banco

---
+++ Documents/workspace-spring-tool-suite-4-4.25.0.RELEASE/loja_estudo_spring/src/main/java/loja/estudo/spring/LojaEstudoSpringApplication.java
@@ -0,0 +1,13 @@
+package loja.estudo.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaEstudoSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojaEstudoSpringApplication.class, args);
+	}
+
+}
---
+++ Documents/workspace-spring-tool-suite-4-4.25.0.RELEASE/loja_estudo_spring/src/test/java/loja/estudo/spring/LojaEstudoSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package loja.estudo.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojaEstudoSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
